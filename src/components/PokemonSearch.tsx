0cc883fcebb5ac6caf205ccf3204ad6c6302652e
Juan Sarmiento <juansarmiento@Mac-mini-de-TeraAdmin-2.local>
2019-09-24 08:03:44 -0500

    first commit

---
+++ src/components/PokemonSearch.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class PokemonSearch extends Component<{name: string, numberOfPokemons: number }> {
+    render() {
+        const { name }
+        return (
+            <div>
+                <p>User </p>
+            </div>
+        )
+    }
+}
+
+export default PokemonSearch
